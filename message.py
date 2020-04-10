8c83a4f7c0d1c6435fb81952c4ebbfbc77a56b92
Zhu Xi <zhuxi910511@163.com>
2020-04-10 16:02:20 +0800

    init repo

---
+++ message.py
@@ -0,0 +1,39 @@
+import struct
+
+message_sig = 0xA187A278
+message_echo_type = 0x1234
+
+parse_error = 0
+buffer_not_enough = 1
+parse_ok = 2
+
+def remove_message(buf):
+    if len(buf) < 12:
+        return buffer_not_enough, None, buf
+
+    buf = ''.join(buf)
+
+    if not struct.unpack('!I', buf[:4])[0] == message_sig:
+        #print struct.unpack('!I', buf[:4])[0]
+        return parse_error, None, list(buf)
+
+    totlen, type = struct.unpack('!II', buf[4:12])
+    #type = struct.unpack('!I', buf[8:12])[0]
+
+    if totlen <= len(buf):
+        data = buf[12:totlen]
+        buf = buf[totlen:]
+        return parse_ok, (type, data), list(buf)
+    else:
+        return buffer_not_enough, None, list(buf)
+
+def extend_message(buf, type, data):
+    message = struct.pack('!3I' + str(len(data)) + 's', message_sig, len(data) + 12, type, data)
+    buf.extend(message)
+    return buf
+
+
+if __name__ == '__main__':
+    buf = extend_message([], message_echo_type, "123131949179374aaiuevavainveoivnaonevaeivaoveivaoeiioaf10112094fhweh")
+    print len(buf)
+
---
+++ message_server.py
@@ -0,0 +1,157 @@
+import socket 
+import select 
+import threading
+
+import message
+
+class MessageServer:
+    def __init__(self, host, port):
+        self.host = host 
+        self.port = port
+        self.epoll = select.epoll()
+        self.fd_to_socket = {}
+        self.client_buffer = {}
+
+        self.mssocket = self.init_server()
+
+    def init_server(self):
+        mssocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        mssocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        address = (self.host, self.port)
+        mssocket.bind(address)
+        mssocket.listen(100)
+
+        self.epoll.register(mssocket.fileno(), select.EPOLLIN)
+        
+        self.fd_to_socket[mssocket.fileno()] = mssocket
+
+        return mssocket
+
+    def process(self, fd, type, data):
+        #print "Process message here"
+        # for test
+        if type == message.message_echo_type:
+            self.add_reply(fd, type, data)
+        else:
+            print "error message type", type
+
+    def close_client(self, fd):
+        self.epoll.unregister(fd)
+        socket = self.fd_to_socket[fd]
+        socket.close()
+        del self.fd_to_socket[fd]
+        del self.client_buffer[fd]
+
+    def add_reply(self, fd, type, data):
+        if data is None or len(data) == 0:
+            return
+        write_buffer = self.client_buffer[fd][1]
+        message.extend_message(write_buffer, type, data)
+        self.epoll.modify(fd, select.EPOLLOUT)
+
+    def run(self):
+
+        print "Start to wait epoll event"
+        while True:
+            events = self.epoll.poll()
+            
+            for fd, event in events:
+                socket = self.fd_to_socket[fd]
+                if socket == self.mssocket:
+                    if not event & select.EPOLLIN:
+                        print "Server error!"
+                        exit(-1)
+                    # new connection
+                    connect_socket, address = self.mssocket.accept()
+                    connect_socket.setblocking(False)
+                    self.epoll.register(connect_socket.fileno(), 
+                            select.EPOLLIN)
+                    self.fd_to_socket[connect_socket.fileno()] = connect_socket
+                    # 1 for read; 2 for write
+                    self.client_buffer[connect_socket.fileno()] = [ [], []] 
+
+                elif event & select.EPOLLHUP:
+                    print "Client close!"
+                    self.close_client(fd)
+
+
+                elif event & select.EPOLLIN:
+                    try:
+                        data = socket.recv(1024)
+                    except socket.error as e:
+                        print e
+
+                    if data: 
+                        read_buffer = self.client_buffer[fd][0]
+                        read_buffer.extend(data) 
+
+                        while True:
+                            status, m, read_buffer = message.remove_message(read_buffer)
+                            self.client_buffer[fd][0] = read_buffer
+
+                            if status == message.parse_ok:
+
+                                type, data = m
+                                # process
+                                self.process(fd, type, data)  
+                            elif status == message.buffer_not_enough:
+                                break 
+                            else:
+                                print "Parse message error, close Client"
+                                self.close_client(fd)
+                                break
+
+                elif event & select.EPOLLOUT:
+                    write_buffer = ''.join(self.client_buffer[fd][1])
+                    nwrite = socket.send(write_buffer)
+                    self.client_buffer[fd][1] = list(write_buffer[nwrite:])
+                    if len(self.client_buffer[fd][1]) == 0:
+                        self.epoll.modify(fd, select.EPOLLIN)
+
+
+
+
+lock = threading.Lock()
+
+def run_test_client():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("127.0.0.1", 12346))
+
+    for i in range(8):
+        id = threading.currentThread().ident
+        data = "this id no is " + str(id) + " %d send time!" % i 
+
+        buf = message.extend_message([], message.message_echo_type, data)
+        #print "send_totlan", len(buf)
+        s.sendall(''.join(buf))
+
+        read_buffer = []
+        target_read_size = len(buf)
+        total_read_size = 0
+        while total_read_size < target_read_size:
+            chunk = s.recv(min(target_read_size - total_read_size, 
+                1024))
+            if len(chunk) == 0:
+                raise RuntimeError("connection broker")
+            read_buffer.extend(chunk)
+            total_read_size += len(chunk)
+        status, m, read_buffer = message.remove_message(read_buffer)
+        lock.acquire()
+        print "receive reply", status, m
+        lock.release()
+
+
+
+if __name__ == '__main__':
+    server = MessageServer("127.0.0.1", 12346)
+
+    for i in range(100):
+        t = threading.Thread(target=run_test_client)
+        t.start()
+
+    server.run()
+
+
+
+
+
